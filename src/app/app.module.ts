c452d4fc527b8b4fd206594e2ba464a02187c8a5
OFFICE\hkaraagac <hakan.karaagac@netnames.com>
2017-08-09 15:31:20 +0100

    Moved hero-details out into it's own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,21 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
+    FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
 
 /*
 Copyright 2017 Google Inc. All Rights Reserved.
