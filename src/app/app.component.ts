47593dffb704dbfe974d6e472f7a45c245898679
OFFICE\hkaraagac <hakan.karaagac@netnames.com>
2017-08-09 14:46:22 +0100

    Add a selected class to the hero link that is selected so we can style it accordingly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,7 +73,9 @@ const HEROES: Hero[] = [
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
       <ul class="heroes">
-        <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
+        <li *ngFor="let hero of heroes" 
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
           <span class="badge">{{hero.id}}</span>{{hero.name}}
         </li>
       </ul> 
@@ -91,6 +93,7 @@ export class AppComponent {
   heroes = HEROES;
   title = 'Tour of Heroes';
   selectedHero: Hero;
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }   
