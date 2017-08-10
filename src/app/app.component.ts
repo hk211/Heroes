b26d985404eb494d15cf30767d8f31d812bf504f
OFFICE\hkaraagac <hakan.karaagac@netnames.com>
2017-08-10 11:24:33 +0100

    Load heroes after 2 seconds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,7 +77,7 @@ export class AppComponent implements OnInit {
 
   getHeroes(): void {
     // Pass the callback function as an argument to the Promise's then() method:
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   ngOnInit(): void {
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,4 +7,11 @@ export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     }
+
+    // Simulate server latency with 2 second delay
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
+    }
 }
